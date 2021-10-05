e4e018453bb3f901f6000494f6b00809e24a60bf
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2021-10-04 19:10:45 -0700

    Time: 162 ms (5.06%), Space: 19.3 MB (12.26%) - LeetHub

---
+++ move-zeroes/move-zeroes.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for(int i = 0, j = 0; i<nums.size(); i++){
+            if(j<i) j = i;
+            while(j<nums.size() && nums[j]==0) j++;cout<<i<<" "<<j<<" ";
+            if(j<nums.size() && i<=j && nums[i]==0) swap(nums[i], nums[j]);
+        }
+    }
+};
