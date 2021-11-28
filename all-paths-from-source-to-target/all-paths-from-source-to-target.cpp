291abe24f0c34f316c9201baf57ccb652fd6bca3
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2021-11-27 16:25:57 -0800

    Time: 24 ms (43.22%), Space: 15.1 MB (39.92%) - LeetHub

---
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> paths;
+        dfs(0, graph, paths, {});
+        return paths;
+    }
+    void dfs(int u, vector<vector<int>>& graph, vector<vector<int>>& paths, vector<int> path) {
+        path.push_back(u);
+        if(u == graph.size()-1) {
+            paths.push_back(path);
+        }
+        for(auto v: graph[u]) {
+            if(find(path.begin(), path.end(), v) == path.end()) {
+                dfs(v, graph, paths, path);
+            }
+        }
+        path.pop_back();
+    }
+};
