98e0b5a510aa5fbc8ad02b6a8162fb65179db7f0
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2021-12-05 10:06:53 -0800

    Time: 8 ms (95.32%), Space: 9.2 MB (60.82%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        if (n == 0) return "";
-        int max_len = 1;
-        int start = 0;
-        for (int i = 0; i < n; i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = max(len1, len2);
-            if (len > max_len) {
-                max_len = len;
-                start = i - (len - 1) / 2;
+        string str = "^#";
+        for (char c : s) {
+            str += c;
+            str += '#';
+        }
+        str += '$';
+        int n = str.size();
+        vector<int> p(n+1, 0);
+        int C = 0, R = 0;
+        for (int i = 1; i < n - 1; i++) {
+            int i_mirror = 2 * C - i;
+            p[i] = (R > i) ? min(p[i_mirror], R - i) : 0;
+            while (str[i + 1 + p[i]] == str[i - 1 - p[i]]) p[i]++;
+            if (i + p[i] > R) {
+                C = i;
+                R = i + p[i];
             }
         }
+        int max_len = 0;
+        int center_index = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (p[i] > max_len) {
+                max_len = p[i];
+                center_index = i;
+            }
+        }
+        int start = (center_index - 1 - max_len) / 2;
         return s.substr(start, max_len);
     }
-
-    int expandAroundCenter(string &s, int left, int right) {
-        int n = s.size();
-        while (left >= 0 && right < n && s[left] == s[right]) {
-            left--;
-            right++;
-        }
-        return right - left - 1;
-    }
-};
+}; // O(n) manacher algorithm
