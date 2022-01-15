c7c30663b53c5a26e40edb1571cd74d105c87701
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-01-15 12:38:52 -0800

    Time: 0 ms (100.00%), Space: 6.1 MB (7.50%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+        return n > 0 && floor(log(n) / log(4)) == ceil(log(n) / log(4));
     }
 };
