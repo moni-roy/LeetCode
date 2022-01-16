b34d2848914a044ce09528bd7723a78b3d437419
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-01-15 23:17:15 -0800

    Time: 547 ms (30.00%), Space: 123 MB (40.00%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -5,17 +5,17 @@ public:
         return func(questions, 0, points);
     }
 private:
-    long long func(vector<vector<int>>& questions, int l, vector<long long>& points) {
-        if (l >= questions.size()) {
+    long long func(vector<vector<int>>& questions, int pos, vector<long long>& points) {
+        if (pos >= questions.size()) {
             return 0;
         }
-        if (points[l] != -1) {
-            return points[l];
+        if (points[pos] != -1) {
+            return points[pos];
         }
         long long res = 0;
-        res = max(res, func(questions, l + 1, points));
-        res = max(res, questions[l][0] + func(questions, l + questions[l][1] + 1, points));
-        points[l] = res;
+        res = max(res, func(questions, pos + 1, points));
+        res = max(res, questions[pos][0] + func(questions, pos + questions[pos][1] + 1, points));
+        points[pos] = res;
         return res;
     }
 };
