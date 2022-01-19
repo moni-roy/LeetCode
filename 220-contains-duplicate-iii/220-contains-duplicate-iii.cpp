f7b2a109e1e1755880e1e8c8e726083686f012ad
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-01-18 20:50:48 -0800

    Time: 58 ms (30.68%), Space: 18.3 MB (65.10%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        return containsNearbyAlmostDuplicateUsingBST(nums, k, t);
+        return containsNearbyAlmostDuplicateUsingBucket(nums, k, t);
     }
 private:
     bool containsNearbyAlmostDuplicateUsingBST(vector<int>& nums, int k, int t) {
@@ -13,6 +13,27 @@ private:
             if (it != bst.end() && *it - nums[i] <= t) return true;
             if (i >= k) bst.erase(bst.find(nums[i - k]));
             bst.insert(nums[i]);
+        }
+        return false;
+    }
+    bool containsNearbyAlmostDuplicateUsingBucket(vector<int>& nums, int k, int t) {
+        if (k < 1 || t < 0) return false;
+        int n = nums.size();
+        unordered_map<long long, long long> bucket;
+
+        auto getIdx = [&](long long x) {
+            long long idx = x / (1LL + t);
+            if (x < 0) idx--;
+            return idx;
+        };
+
+        for (int i = 0; i < n; i++) {
+            int idx = getIdx(nums[i]);
+            if (bucket.find(idx) != bucket.end()) return true;
+            if (bucket.find(idx - 1) != bucket.end() && nums[i] - bucket[idx - 1] <= t) return true;
+            if (bucket.find(idx + 1) != bucket.end() && bucket[idx + 1] - nums[i] <= t) return true;
+            bucket[idx] = nums[i];
+            if (i >= k) bucket.erase(bucket.find(getIdx(nums[i - k])));
         }
         return false;
     }
