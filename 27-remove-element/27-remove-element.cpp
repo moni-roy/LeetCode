b9583e2d20dc4bee7673bcf850a0a0508b2b9f78
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-01-19 22:45:59 -0800

    Time: 5 ms (15.01%), Space: 8.8 MB (72.74%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+      return removeElementUsingTwoPointer(nums, val);
+    }
+private:
+    int removeElementUsingTwoPointer(vector<int> &nums, int val) {
+      int left = 0, right = nums.size()-1;
+      while(left <= right) {
+        if(nums[left] == val) {
+          swap(nums[left], nums[right]);
+          right--;
+        }
+        else left++;
+      }
+      return left;
+    }
+};
