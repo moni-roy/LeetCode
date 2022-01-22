0765b0b6da939f5d90a424eb352819be10da4016
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-01-21 21:41:40 -0800

    Time: 1479 ms (5.03%), Space: 35.8 MB (8.72%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n+1, false);
+        vector<bool> visited(n+1, false);
+        return winnerSquareGameUsingDP(n, dp, visited);
+    }
+private:
+    bool winnerSquareGameUsingDP(int n, vector<bool> &dp, vector<bool> &visited) {
+        if(n == 1) return true;
+        if(n == 2) return false;
+        if(visited[n]) return dp[n];
+        for(int i = 1; i*i <= n; i++) {
+            dp[n] = dp[n] | !winnerSquareGameUsingDP(n - i*i, dp, visited);
+        }
+        visited[n] = true;
+        return dp[n];
+    }
+};
