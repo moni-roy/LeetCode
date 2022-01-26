22e266e53f11741512664414bb9c4d6830aca25f
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-01-25 17:16:44 -0800

    Time: 187 ms (43.85%), Space: 86.8 MB (15.36%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> ans = usingInorder(root1, root2);
+        return ans;
+    }
+private:
+    vector<int> usingInorder(TreeNode* root1, TreeNode* root2) {
+        vector<int> inorderRoot1;
+        inorder(root1, inorderRoot1);
+        vector<int> inorderRoot2;
+        inorder(root2, inorderRoot2);
+        int len1 = inorderRoot1.size(), len2 = inorderRoot2.size();
+        vector<int> ans;
+        int i = 0, j = 0;
+        while(i < len1 && j < len2) {
+            if(inorderRoot1[i] <= inorderRoot2[j]) {
+                ans.push_back(inorderRoot1[i++]);
+            }
+            else {
+                ans.push_back(inorderRoot2[j++]);
+            }
+        }
+        while(i<len1) ans.push_back(inorderRoot1[i++]);
+        while(j<len2) ans.push_back(inorderRoot2[j++]);
+        return ans;
+    }
+    
+    void inorder(TreeNode *root, vector<int> &values) {
+        if(!root) return;
+        inorder(root->left, values);
+        values.push_back(root->val);
+        inorder(root->right, values);
+    }
+};
