cf2d3dc3ad10f635308c7ddfdab92ba1dce51583
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-01-26 21:55:38 -0800

    Time: 691 ms (25.07%), Space: 317 MB (5.02%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,7 +1,17 @@
+struct TrieNode {
+    TrieNode* children[26];
+    TrieNode() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+    }
+};
+
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        return usingHashSet(nums);
+        // return usingHashSet(nums);
+        return usingTrie(nums);
     }
 private:
     int usingHashSet(vector<int> &nums) {
@@ -19,6 +29,35 @@ private:
                     break;
                 }
             }
+        }
+        return maxPrefix;
+    }
+    int usingTrie(vector<int> &nums) {
+        TrieNode *root = new TrieNode();
+        for (int num : nums) {
+            TrieNode *curr = root;
+            for (int i = 31; i >= 0; --i) {
+                int bit = (num >> i) & 1;
+                if (!curr->children[bit]) {
+                    curr->children[bit] = new TrieNode();
+                }
+                curr = curr->children[bit];
+            }
+        }
+        int maxPrefix = 0;
+        for (int num : nums) {
+            TrieNode *curr = root;
+            int currPrefix = 0;
+            for (int i = 31; i >= 0; --i) {
+                int bit = (num >> i) & 1;
+                if (curr->children[1 - bit]) {
+                    currPrefix = currPrefix | (1 << i);
+                    curr = curr->children[1 - bit];
+                } else {
+                    curr = curr->children[bit];
+                }
+            }
+            maxPrefix = max(maxPrefix, currPrefix);
         }
         return maxPrefix;
     }
