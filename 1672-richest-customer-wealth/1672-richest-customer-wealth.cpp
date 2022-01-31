730126d82702aa2912dc0ff93376314121b65a9a
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-01-30 18:19:38 -0800

    Time: 4 ms (90.57%), Space: 8.2 MB (8.24%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -4,7 +4,7 @@ public:
         int ans = 0;
         for(auto account: accounts) {
             int total = accumulate(account.begin(), account.end(), 0);
-            ans = ans < total ? total : ans;
+            ans = max(ans, total);
         }
         return ans;
     }
