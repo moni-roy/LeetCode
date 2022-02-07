c2f7bcb6459fb5253010e72f0d64730577a558b1
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-02-06 18:49:39 -0800

    Time: 4 ms (67.41%), Space: 6.9 MB (9.15%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> m;
+        for (char c : s) {
+            m[c]++;
+        }
+        for (char c : t) {
+            if (m[c] == 0) {
+                return c;
+            }
+            m[c]--;
+        }
+        return ' ';
+    }
+};
