519ad4177158f58697dce31e8b555d0c9de9528a
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-02-24 22:30:40 -0800

    Time: 36 ms (44.66%), Space: 63.5 MB (17.54%) - LeetHub

---
+++ 77-combinations/77-combinations.java
@@ -0,0 +1,35 @@
+public class Solution {
+    HashMap<String, List<List<Integer>>> map = new HashMap<>();
+    public List<List<Integer>> combine(int n, int k) {
+		List<List<Integer>> list = new LinkedList<List<Integer>>();
+		if(k<1) return list;
+		return combine(n, k, 1);
+	}
+	
+	private List<List<Integer>> combine(int n, int k, int start) {
+		List<List<Integer>> list = new LinkedList<List<Integer>>();
+		if(k<1) return list;		
+		if(map.containsKey(k+":"+n+":"+start)) return map.get(k+":"+n+":"+start);
+		
+		for(int i=start;i<=n;i++){
+			if(k==1){
+				List<Integer> l = new LinkedList<Integer>();
+				l.add(i);
+				list.add(l);
+			}
+			else{
+				List<List<Integer>> sub = combine(n, k-1, i+1);
+				if(sub!=null){
+					for(List<Integer> l: sub){
+						List<Integer> ll = new LinkedList<Integer>();
+						ll.addAll(l);
+						ll.add(0, i);
+						list.add(ll);
+					}
+				}
+			}
+		}
+		map.put(k+":"+n+":"+start, list);
+		return list;
+	}
+}
