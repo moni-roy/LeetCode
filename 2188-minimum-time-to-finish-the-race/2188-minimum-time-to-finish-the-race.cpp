221800ca6c22197e26ce19aac4459d999611239e
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-02-26 21:00:52 -0800

    Time: 632 ms (45.45%), Space: 205.4 MB (18.18%) - LeetHub

--- 2188-minimum-time-to-finish-the-race/2188-minimum-time-to-finish-the-race.cpp
+++ 2188-minimum-time-to-finish-the-race/2188-minimum-time-to-finish-the-race.cpp
@@ -3,12 +3,12 @@ public:
     int minimumFinishTime(vector<vector<int>>& tires, int changeTime, int numLaps) { 
         // pre-processing
         int maxLap = 0;
-        vector<long long> bestLap(30 + 1, LONG_LONG_MAX);
+        vector<long long> bestLap(20 + 1, LONG_LONG_MAX);
         for(auto tire : tires) {
             long long F = tire[0];
             long long R = tire[1];
             long long cost = 0;
-            for(int i = 1; i <= 30 && F < INT_MAX; i++) {
+            for(int i = 1; i <= 20 && F < INT_MAX; i++) {
                 cost += F;
                 bestLap[i] = min(bestLap[i], cost);
                 F *= R;
