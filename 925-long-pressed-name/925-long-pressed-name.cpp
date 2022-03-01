d6eea263d1c19a83a266bf3f9038f731529ec56b
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-02-28 21:01:40 -0800

    Time: 0 ms (100.00%), Space: 6.4 MB (61.13%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.cpp
+++ 925-long-pressed-name/925-long-pressed-name.cpp
@@ -2,18 +2,11 @@ class Solution {
 public:
     bool isLongPressedName(string name, string typed) {
         int i = 0, j = 0;
-        while (i < name.size() && j < typed.size()) {
-            if (name[i] == typed[j]) {
+        while (i < name.size() || j < typed.size()) {
+            if (i < name.size() && name[i] == typed[j]) {
                 i++;
                 j++;
             } else if (j > 0 && typed[j] == typed[j - 1]) {
-                j++;
-            } else {
-                return false;
-            }
-        }
-        while(j < typed.size()) {
-            if (j > 0 && typed[j] == typed[j - 1]) {
                 j++;
             } else {
                 return false;
