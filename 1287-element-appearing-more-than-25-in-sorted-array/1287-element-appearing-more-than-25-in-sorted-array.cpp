f6f419267073114370b4d4d9a7569bf53dbf88a5
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-02-28 21:13:17 -0800

    Time: 8 ms (97.88%), Space: 12.4 MB (94.13%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
+        return withoutExtraSpace(arr);
+    }
+private:
+    int usingExtraSpace(vector<int>& arr) {
         unordered_map<int, int> freq;
         for(auto a: arr) freq[a]++;
         int freq25 = arr.size() / 4;
         for(auto it: freq) {
             if(it.second > freq25) return it.first;
+        }
+        return 1;
+    }
+    
+    int withoutExtraSpace(vector<int>& arr) {
+        int n = arr.size();
+        int cnt = n / 4;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == arr[i + cnt]) {
+                return arr[i];
+            }
         }
         return 1;
     }
