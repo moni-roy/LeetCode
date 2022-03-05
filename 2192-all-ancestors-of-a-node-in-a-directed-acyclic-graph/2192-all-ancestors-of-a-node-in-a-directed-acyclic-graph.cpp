7abdd1085da62d55b5248906ba49a2da77b1628b
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-03-05 13:24:19 -0800

    Time: 210 ms (42.86%), Space: 62.8 MB (57.14%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        return getAncestorsGraph(n, edges);
+    }
+private:
+    vector<vector<int>> getAncestorsGraph(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph(n);
+        for (auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+        }
+        vector<vector<int>> ancestors(n);
+        for (int i = 0; i < n; ++i) {
+            vector<bool> visited(n, false);
+            getAncestors(i, i, graph, ancestors, visited);
+        }
+        return ancestors;
+    }
+
+    void getAncestors(int node, int parent, vector<vector<int>>& graph, vector<vector<int>>& ancestors, vector<bool>& visited) {
+        visited[node] = true;
+        for (int child : graph[node]) {
+            if (!visited[child]) {
+                ancestors[child].push_back(parent);
+                getAncestors(child, parent, graph, ancestors, visited);
+            }
+        }
+    }
+
+    // using reverse graph
+    vector<vector<int>> getAncestorsReverseGraph(int n, vector<vector<int>>& edges) {
         vector<vector<int>> res(n, vector<int>());
         vector<vector<int>> graph(n, vector<int>());
         for (auto& edge : edges) {
@@ -15,12 +43,12 @@ public:
         }
         return res;
     }
-private:
+
     void dfs(int node, vector<vector<int>>& graph, vector<vector<int>>& res, int parent, vector<bool>& visited) {
         visited[node] = true;
-        if(parent != node) res[parent].push_back(node);
         for (auto& child : graph[node]) {
             if (!visited[child]) {
+                res[parent].push_back(child);
                 dfs(child, graph, res, parent, visited);
             }
         }
