fa8139029e0279d24627d2f5e8287d5b8fd1765f
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-03-11 17:16:52 -0800

    Time: 108 ms, Memory: 51.3 MB - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int> mp;
-        for(auto n: nums) {
-            if(mp.find(n) != mp.end()) return true;
-            mp[n] = 1;
+        return containsDuplicateUsingMap(nums);
+        return containsDuplicateUsingSet(nums);
+    }
+private:
+    bool containsDuplicateUsingSet(vector<int>& nums) {
+        unordered_set<int> hashSet;
+        for (int i = 0; i < nums.size(); i++) {
+            if (hashSet.find(nums[i]) != hashSet.end()) {
+                return true;
+            }
+            hashSet.insert(nums[i]);
+        }
+        return false;
+    }
+    
+    bool containsDuplicateUsingMap(vector<int>& nums) {
+        unordered_map<int, int> mapCount;
+        for(auto num: nums) {
+            mapCount[num] ++;
+            if(mapCount[num] == 2) return true;
         }
         return false;
     }
