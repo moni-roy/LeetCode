e553438a1be930ca9536de43473b610c2939706f
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-04-10 18:51:44 -0700

    Time: 6 ms (40.91%), Space: 5.9 MB (27.83%) - LeetHub

---
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+		long long int left = 0, right = sqrt(c);
+		while (left <= right) {
+			long long int sum = left * left + right * right;
+			if (sum == c) return true;
+			else if (sum < c) left++;
+			else right--;
+		}
+		return false;
+	}
+};
