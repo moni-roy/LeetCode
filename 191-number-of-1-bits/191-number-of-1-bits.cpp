98c683b41f11dc921c5a81cbab096d6799498c5b
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-05-25 18:58:04 -0700

    Time: 0 ms (100.00%), Space: 5.8 MB (96.65%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int ret = 0;
         while(n) {
-            ret += n%2;
-            n = n/2;
+            ret += n % 2;
+            n = n / 2;
         }
         return ret;
     }
