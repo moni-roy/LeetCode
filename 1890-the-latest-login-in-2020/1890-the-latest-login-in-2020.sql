54a6228ec81e9f02574cf5718402362bb0870804
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-06-25 20:15:48 -0700

    Time: 619 ms (65.49%), Space: 0B (100.00%) - LeetHub

---
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -0,0 +1,10 @@
+-- Write an SQL query to report the latest login for all users in the year 2020.Do not include the users who did not login in 2020.
+SELECT
+    user_id,
+    MAX(time_stamp) AS last_stamp
+FROM
+    logins
+WHERE
+    time_stamp LIKE '2020%'
+GROUP BY
+    user_id;
