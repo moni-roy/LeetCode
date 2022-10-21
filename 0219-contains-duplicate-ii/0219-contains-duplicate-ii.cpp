27168de8fb2fa441663183fa0dc20851d4f86bae
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2022-10-20 18:27:46 -0700

    Time: 414 ms (35.53%), Space: 77.2 MB (60.05%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        return containsNearbyDuplicateUsingMap(nums, k);
+    }
+private:
+    bool containsNearbyDuplicateUsingMap(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.find(nums[i]) != m.end() && i - m[nums[i]] <= k) {
+                return true;
+            }
+            m[nums[i]] = i;
+        }
+        return false;
+    }
+};
