3cd80c27434a0a6d8991b78797e5b91b00a1b3ae
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2023-08-21 20:51:54 -0700

    Time: 2 ms (49.78%), Space: 5.9 MB (37.93%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        if(columnNumber == 1) return "A";
+        string ans = "";
+        while(columnNumber) {
+            columnNumber--;
+            ans += (char)(columnNumber % 26 + 'A');
+            columnNumber /= 26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
