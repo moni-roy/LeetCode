4909e38b6d6104380528f1672366ebfc840dbfac
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2023-10-14 18:37:14 -0700

    Time: 26 ms (45.67%), Space: 13.2 MB (53.03%) - LeetHub

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numWays(int steps, int arrLen) {
+        arrLen = min(steps, arrLen);
+        vector <vector<int> > dp = vector(arrLen, vector(steps + 1, -1));
+        return fun(0, steps, arrLen, dp);
+    }
+private:
+    const int mod = 1e9 + 7;
+    int fun(int pos, int steps, int arrLen, vector<vector<int> > & dp) {
+        if (steps == 0) return pos == 0;
+        if (dp[pos][steps] != -1) return dp[pos][steps];
+        int ret = fun(pos, steps - 1, arrLen, dp);
+        if (pos > 0) ret = (ret + fun(pos - 1, steps - 1, arrLen, dp)) % mod;
+        if (pos < arrLen - 1) ret = (ret + fun(pos + 1, steps - 1, arrLen, dp)) % mod;
+        return dp[pos][steps] = ret;
+    }
+};
