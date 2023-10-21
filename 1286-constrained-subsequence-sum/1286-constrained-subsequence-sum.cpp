2ea80598a379fb6c6827ab013876c5d273ddb101
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2023-10-20 19:50:03 -0700

    Time: 195 ms (90.2%), Space: 122.5 MB (61.63%) - LeetHub

---
+++ 1286-constrained-subsequence-sum/1286-constrained-subsequence-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int constrainedSubsetSum(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> dp(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            if (!dq.empty() && i - dq.front() > k) dq.pop_front();
+            dp[i] = (dq.empty() ? 0 : dp[dq.front()]) + nums[i];
+            while (!dq.empty() && dp[dq.back()] < dp[i]) dq.pop_back();
+            if (dp[i] > 0) dq.push_back(i);
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
